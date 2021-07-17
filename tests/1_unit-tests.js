3c625c057fdbca8fa522ef403751ea2497c634e0
WiseGuise <79070546+WiseGuise@users.noreply.github.com>
2021-07-17 05:14:04 -0500

    Completed
    Completed project - passing all tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -4,6 +4,37 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 
 let convertHandler = new ConvertHandler();
 
-suite('Unit Tests', function(){
+suite( 'Unit Tests', function () {
+    
+    suite( "Function convertHandler.getNum(input)", function () {
+        
+        test( "Whole Number Input", function ( done ) {
+            let input = "32L";
+            assert.equal( convertHandler.getNum( input ), 32 );
+            done();
+        } );
+
+        test( "Decimal Input", function ( done ) {
+            let input = "32.2L";
+            assert.equal( convertHandler.getNum( input ), 32.2 );
+            done();
+        } );
+
+        test( "Fractional Input", function ( done ) {
+            let input = "1/32L";
+            assert.equal( convertHandler.getNum( input ), 1 / 32 );
+            done();
+        } );
+
+        test( "Fractional Input w/ Decimal", function ( done ) {
+            let input = "1.2/32L";
+            assert.equal( convertHandler.getNum( input ), 1.2 / 32 );
+            done();
+        } );
+
+        test("")
+
+
+    })
 
 });
